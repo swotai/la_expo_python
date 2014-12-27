5467bdc5bed505600559ddc1227678db9912084f
Dennis Tai <kdt43@cornell.edu>
2014-12-27 15:00:06 -0800

    Setup calcTransit file to calculate transit change

---
+++ Python/GIS_CalcTransit.py
@@ -0,0 +1,141 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec 23 21:38:08 2014
+
+@author: Dennis
+# Purpose: Run modules to do the following:
+# 3, Calculate cost (TD + DT)
+# 4, Compute station level flow
+
+# For Equilibrium calculation
+
+"""
+
+# Name: GIS_Iteration
+
+
+if __name__ == '__main__':
+    # Parameter settings (mostly paths):
+    # NOTE: MAKE SURE inSpace folder HAS ending slash "/"
+    base = "DriveOnly_LA.gdb"
+    temp = "LA-scratch.gdb"
+    #inSpace = "C:/Users/Dennis/Desktop/DATA1/"
+    inSpace = "E:/DATA_Post_fix1028/"
+    inGdb = temp
+    #inSpeed = "spdtest.csv"
+#    SET whether use transit Pre or Post cost
+#    Make sure csvs have correct format.
+#    oID_TAZ12A,dID_TAZ12A,post_triptime,postcost
+#    20211000,20211000,0,0
+#    20211000,20212000,89.348434,19.777
+    inTTp = inSpace+"TTpubPre.csv" 
+    inTTp = inSpace+"TTpubPost.csv"
+
+    
+    base = inSpace+base
+    temp = inSpace+temp
+    
+    ##TAZs:
+    ##    TAZ_LA_proj_centroid  >---< inFlow.csv
+    ##    TAZ_LA_TESTSAMPLE (50)  >---< inFlow50.csv
+    ##Detectors:
+    ##    hd_ML_snap (1800)
+    ##Stations:
+    ##    ??? (20?)
+    fcTAZ = "TAZ_LA_proj_centroid"
+    fcDet = "hd_ML_snap"
+    #inFLOW = PREDICTED FLOW FROM GRAVITY (STATIC, OBSELETE), not to confused with AF (Actual Flow)
+    #inFLOW.csv at inSpace is used for Pre flow as starting point for iteration.
+#    inFlow = inSpace + "inFlow.csv"
+    
+    #Specify output file path for excel of relative gap 
+    outRelGap = inSpace + "FIGS/relgap.csv"
+    
+        
+    #For Allocation
+    #weight is how much weight to put into new speed
+    #FL should be 500, however because small TAZ number
+    #in test sample, threshold changed to 1 to test equation.
+    LIMIT = 65
+    weight = 0.5
+    FL = 500
+    
+    # Iteration ending thresholds:
+    threshIT = 0.00001
+    threshDS = 1
+    
+    # The way this is coded, if an iteraction is completed
+    # i.e. with the speed outputed, the code can start from there.
+    # Change the currentIter to the max number of detspd +1
+    
+
+    #ACTUAL COMPUTATION START HERE
+    # Import necessary modules
+    import ModSetupWorker, ModJoinSpeed, ModBuild, ModSolve, ModAlloc, ModSpeedCalc_avg10, ModUpdateFlow
+    import time
+    
+    currentIter = 1
+    
+    # Pre equilibrium calculation
+    print "Pre transit calculation starts on", time.strftime("%d/%m/%Y - %H:%M:%S")
+    inSpace = "C:/Users/Dennis/Desktop/transPre/"
+
+    # Specify the transit gdb
+    base = "DriveOnly_LA.gdb"
+    temp = "LA-scratch.gdb"
+    #inSpace = "C:/Users/Dennis/Desktop/DATA1/"
+    inSpace = "E:/DATA_Post_fix1028/"
+    inGdb = temp
+
+    # 0, create temp scratch
+    print "Setting up scratch version"
+    ModSetupWorker.clearOld(base,temp)
+    print "Scratch version set up.  Proceding..."
+
+    # 2, rebuild network dataset
+    print "Speed updated. Rebuild Dataset..."
+    ModBuild.build(inSpace, inGdb)
+    
+    # 3, solve network
+    print "Dataset rebuilt. Solve for TT, TD, DT"
+    ModSolve.solvetrans(inSpace, inGdb, fcTAZ, fcDet)
+
+    print "Flow Allocation"
+    inFlow = inSpace + "CSV/TTflow" + str(currentIter) + ".csv"
+    flow = ModAlloc.alloc(inSpace, inFlow, currentIter)
+
+    print "Pre transit calculation completes on", time.strftime("%d/%m/%Y - %H:%M:%S")
+
+
+
+
+    # Pre equilibrium calculation
+    print "Post transit calculation starts on", time.strftime("%d/%m/%Y - %H:%M:%S")
+    inSpace = "C:/Users/Dennis/Desktop/transPre/"
+
+    # Specify the transit gdb
+    base = "DriveOnly_LA.gdb"
+    temp = "LA-scratch.gdb"
+    #inSpace = "C:/Users/Dennis/Desktop/DATA1/"
+    inSpace = "E:/DATA_Post_fix1028/"
+    inGdb = temp
+
+    # 0, create temp scratch
+    print "Setting up scratch version"
+    ModSetupWorker.clearOld(base,temp)
+    print "Scratch version set up.  Proceding..."
+
+    # 2, rebuild network dataset
+    print "Speed updated. Rebuild Dataset..."
+    ModBuild.build(inSpace, inGdb)
+    
+    # 3, solve network
+    print "Dataset rebuilt. Solve for TT, TD, DT"
+    ModSolve.solvetrans(inSpace, inGdb, fcTAZ, fcDet)
+
+    print "Flow Allocation"
+    inFlow = inSpace + "CSV/TTflow" + str(currentIter) + ".csv"
+    flow = ModAlloc.alloc(inSpace, inFlow, currentIter)
+
+    print "Post transit calculation completes on", time.strftime("%d/%m/%Y - %H:%M:%S")
+
--- Python/ModSolve.py
+++ Python/ModSolve.py
@@ -1,101 +1,142 @@
 # Name: ModSolve.py
 # Description: Setup OD cost matrix, solve for TT, TD, DT costs
 #              output cost matrices as CSCV
-# Requirements: Network Analyst Extension 
+# Requirements: Network Analyst Extension
 # The following are hard coded:
-#		- Name of output csv
+#        - Name of output csv
 
 
 from ModRealSolve import NAtoCSV
 import time
 
-def solve(inSpace, inGdb, fcTAZ, fcDet):
-	try:
-		
-		#Set local variables
-		inNetworkDataset = "Trans/DriveOnly_ND"
-		impedanceAttribute = "Cost"
-		accumulateAttributeName = "#"
-		#accumulateAttributeName = ['Length', 'dps']
-		
-		#TT COST CALCULATION STARTS HERE
-		outNALayerName = "ODTT"
-		inOrigins = "Trans/"+fcTAZ
-		inDestinations = "Trans/"+fcTAZ
-		outFile = inSpace+"CSV/TT.csv"
-		NAtoCSV(inSpace, inGdb, inNetworkDataset, impedanceAttribute, accumulateAttributeName, inOrigins, inDestinations, outNALayerName, outFile)
-		print "TT Solved"
-				
-		
-		#TD COST CALCULATION STARTs HERE
-		outNALayerName = "ODTD"
-		inOrigins = "Trans/"+fcTAZ
-		inDestinations = "Trans/"+fcDet
-		outFile = inSpace+"CSV/TD.csv"
-		NAtoCSV(inSpace, inGdb, inNetworkDataset, impedanceAttribute, accumulateAttributeName, inOrigins, inDestinations, outNALayerName, outFile)
-		print "TD Solved"
-		
 
-		#DT COST CALCULATION STARTS HERE
-		outNALayerName = "ODTT"
-		inOrigins = "Trans/"+fcDet
-		inDestinations = "Trans/"+fcTAZ
-		outFile = inSpace+"CSV/DT.csv"
-		NAtoCSV(inSpace, inGdb, inNetworkDataset, impedanceAttribute, accumulateAttributeName, inOrigins, inDestinations, outNALayerName, outFile)
-		print "DT Solved"
-		
-		print "\n\n Solve: CHECK GDB LOCK NOW!!!"
-		
-		
-	except Exception as e:
-		# If an error occurred, print line number and error message
-		import sys
-		tb = sys.exc_info()[2]
-		print "An error occurred in ModSolve line %i" % tb.tb_lineno
-		print str(e)		
+def solve(inSpace, inGdb, fcTAZ, fcDet):
+    '''
+    Solves TT, TD and DT, given fcTAZ and fcDet
+    '''
+    try:
+
+        #Set local variables
+        inNetworkDataset = "Trans/DriveOnly_ND"
+        impedanceAttribute = "Cost"
+        accumulateAttributeName = "#"
+        #accumulateAttributeName = ['Length', 'dps']
+
+        #TT COST CALCULATION STARTS HERE
+        outNALayerName = "ODTT"
+        inOrigins = "Trans/"+fcTAZ
+        inDestinations = "Trans/"+fcTAZ
+        outFile = inSpace+"CSV/TT.csv"
+        NAtoCSV(inSpace, inGdb, inNetworkDataset, impedanceAttribute, accumulateAttributeName, inOrigins, inDestinations, outNALayerName, outFile)
+        print "TT Solved"
+
+
+        #TD COST CALCULATION STARTs HERE
+        outNALayerName = "ODTD"
+        inOrigins = "Trans/"+fcTAZ
+        inDestinations = "Trans/"+fcDet
+        outFile = inSpace+"CSV/TD.csv"
+        NAtoCSV(inSpace, inGdb, inNetworkDataset, impedanceAttribute, accumulateAttributeName, inOrigins, inDestinations, outNALayerName, outFile)
+        print "TD Solved"
+
+
+        #DT COST CALCULATION STARTS HERE
+        outNALayerName = "ODTT"
+        inOrigins = "Trans/"+fcDet
+        inDestinations = "Trans/"+fcTAZ
+        outFile = inSpace+"CSV/DT.csv"
+        NAtoCSV(inSpace, inGdb, inNetworkDataset, impedanceAttribute, accumulateAttributeName, inOrigins, inDestinations, outNALayerName, outFile)
+        print "DT Solved"
+        print "\n\n Solve: CHECK GDB LOCK NOW!!!"
+
+    except Exception as e:
+        # If an error occurred, print line number and error message
+        import sys
+        tb = sys.exc_info()[2]
+        print "An error occurred in ModSolve.sole line %i" % tb.tb_lineno
+        print str(e)
+
+
+def solvetrans(inSpace, inGdb, fcTAZ, fcDet):
+    '''
+    Solves TD and DT, given fcTAZ and fcDet
+    '''
+    try:
+
+        #Set local variables
+        inNetworkDataset = "Trans/DriveOnly_ND"
+        impedanceAttribute = "Cost"
+        accumulateAttributeName = "#"
+        #accumulateAttributeName = ['Length', 'dps']
+
+        #TD COST CALCULATION STARTs HERE
+        outNALayerName = "ODTD"
+        inOrigins = "Trans/"+fcTAZ
+        inDestinations = "Trans/"+fcDet
+        outFile = inSpace+"CSV/TD.csv"
+        NAtoCSV(inSpace, inGdb, inNetworkDataset, impedanceAttribute, accumulateAttributeName, inOrigins, inDestinations, outNALayerName, outFile)
+        print "TD Solved"
+
+
+        #DT COST CALCULATION STARTS HERE
+        outNALayerName = "ODTT"
+        inOrigins = "Trans/"+fcDet
+        inDestinations = "Trans/"+fcTAZ
+        outFile = inSpace+"CSV/DT.csv"
+        NAtoCSV(inSpace, inGdb, inNetworkDataset, impedanceAttribute, accumulateAttributeName, inOrigins, inDestinations, outNALayerName, outFile)
+        print "DT Solved"
+
+        print "\n\n Solve: CHECK GDB LOCK NOW!!!"
+
+    except Exception as e:
+        # If an error occurred, print line number and error message
+        import sys
+        tb = sys.exc_info()[2]
+        print "An error occurred in ModSolve.soletrans line %i" % tb.tb_lineno
+        print str(e)
 
 
 def solve_2p(inSpace, inGdb, fcTAZ, fcDet, period):
-	try:
-		
-		#Set local variables
-		inNetworkDataset = "Trans/DriveOnly_ND"
-		impedanceAttribute = "Cost"
-		accumulateAttributeName = "#"
-		#accumulateAttributeName = ['Length', 'dps']
-		
-		#TT COST CALCULATION STARTS HERE
-		outNALayerName = "ODTT"
-		inOrigins = "Trans/"+fcTAZ
-		inDestinations = "Trans/"+fcTAZ
-		outFile = inSpace+"CSV/TT-" + period + ".csv"
-		NAtoCSV(inSpace, inGdb, inNetworkDataset, impedanceAttribute, accumulateAttributeName, inOrigins, inDestinations, outNALayerName, outFile)
-		print "TT Solved"
-				
-		
-		#TD COST CALCULATION STARTs HERE
-		outNALayerName = "ODTD"
-		inOrigins = "Trans/"+fcTAZ
-		inDestinations = "Trans/"+fcDet
-		outFile = inSpace+"CSV/TD-" + period + ".csv"
-		NAtoCSV(inSpace, inGdb, inNetworkDataset, impedanceAttribute, accumulateAttributeName, inOrigins, inDestinations, outNALayerName, outFile)
-		print "TD Solved"
-		
+    try:
 
-		#DT COST CALCULATION STARTS HERE
-		outNALayerName = "ODTT"
-		inOrigins = "Trans/"+fcDet
-		inDestinations = "Trans/"+fcTAZ
-		outFile = inSpace+"CSV/DT-" + period + ".csv"
-		NAtoCSV(inSpace, inGdb, inNetworkDataset, impedanceAttribute, accumulateAttributeName, inOrigins, inDestinations, outNALayerName, outFile)
-		print "DT Solved"
-		
-		print "\n\n Solve: CHECK GDB LOCK NOW!!!"
-		
-		
-	except Exception as e:
-		# If an error occurred, print line number and error message
-		import sys
-		tb = sys.exc_info()[2]
-		print "An error occurred in ModSolve line %i" % tb.tb_lineno
-		print str(e)		
+        #Set local variables
+        inNetworkDataset = "Trans/DriveOnly_ND"
+        impedanceAttribute = "Cost"
+        accumulateAttributeName = "#"
+        #accumulateAttributeName = ['Length', 'dps']
+
+        #TT COST CALCULATION STARTS HERE
+        outNALayerName = "ODTT"
+        inOrigins = "Trans/"+fcTAZ
+        inDestinations = "Trans/"+fcTAZ
+        outFile = inSpace+"CSV/TT-" + period + ".csv"
+        NAtoCSV(inSpace, inGdb, inNetworkDataset, impedanceAttribute, accumulateAttributeName, inOrigins, inDestinations, outNALayerName, outFile)
+        print "TT Solved"
+
+
+        #TD COST CALCULATION STARTs HERE
+        outNALayerName = "ODTD"
+        inOrigins = "Trans/"+fcTAZ
+        inDestinations = "Trans/"+fcDet
+        outFile = inSpace+"CSV/TD-" + period + ".csv"
+        NAtoCSV(inSpace, inGdb, inNetworkDataset, impedanceAttribute, accumulateAttributeName, inOrigins, inDestinations, outNALayerName, outFile)
+        print "TD Solved"
+
+
+        #DT COST CALCULATION STARTS HERE
+        outNALayerName = "ODTT"
+        inOrigins = "Trans/"+fcDet
+        inDestinations = "Trans/"+fcTAZ
+        outFile = inSpace+"CSV/DT-" + period + ".csv"
+        NAtoCSV(inSpace, inGdb, inNetworkDataset, impedanceAttribute, accumulateAttributeName, inOrigins, inDestinations, outNALayerName, outFile)
+        print "DT Solved"
+
+        print "\n\n Solve: CHECK GDB LOCK NOW!!!"
+
+
+    except Exception as e:
+        # If an error occurred, print line number and error message
+        import sys
+        tb = sys.exc_info()[2]
+        print "An error occurred in ModSolve.solve_2p line %i" % tb.tb_lineno
+        print str(e)
